dd3e27d275d3ca014d0f45f579d99602585c275c
AllaA_d2w3vtn <AllaA_d2w3vtn@LAPTOP-MFO7HJ67>
2024-01-23 12:11:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,14 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+        std::cout << "Розы красные,\n";
+        std::cout << "Фиалки синие,\n";
+        std::cout << "Сахар сладкий\n";
+        std::cout << "И ты тоже.\n";
+}
+
